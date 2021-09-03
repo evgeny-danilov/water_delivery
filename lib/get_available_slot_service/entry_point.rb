e3a93c0b64e2835abc09d4e89b70cfed00b8056b
Evgenii Danilov <evgenii.danilov@smartpension.co.uk>
2021-09-03 09:55:07 +0300

    Step #5 - clean up the code

--- lib/get_available_slot_service/entry_point.rb
+++ lib/get_available_slot_service/entry_point.rb
@@ -2,13 +2,14 @@
 
 module GetAvailableSlotService
   class EntryPoint
+    SLOT_KEY_SPLITTER = '$'
+
     def initialize(params:)
       @params = params
     end
 
     def call
-      # byebug
-      available_slot_params = available_slots.keys.first.split('$')
+      available_slot_params = available_slots.keys.first.split(SLOT_KEY_SPLITTER)
 
       { date: available_slot_params.first, time_period: available_slot_params.last }
     end
@@ -23,9 +24,22 @@ module GetAvailableSlotService
 
     def available_slots
       booked_slots
-        .select { |slot| slot[:date] == params[:date] && slot[:time_period] >= params[:time_period] }
-        .count_by { |slot| [slot[:date], slot[:time_period]].join('$') }
-        .select { |_slot_key, count| count < 2 }
+        .select(&method(:by_user_request))
+        .count_by(&method(:slot_key))
+        .select(&method(:less_than_two_booked_slots))
+    end
+
+    def by_user_request(slot)
+      slot[:date] == params[:date] &&
+        slot[:time_period] >= params[:time_period]
+    end
+
+    def slot_key(slot)
+      [slot[:date], slot[:time_period]].join(SLOT_KEY_SPLITTER)
+    end
+
+    def less_than_two_booked_slots(_slot_key, count)
+      count < 2
     end
   end
 end
